587deec207383ed5ca2e1b2dc37f7664e455f28c
Emmanuel Popoola <olaayo10@gmail.com>
2019-08-21 03:00:55 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import colorsHelper from "./colorsHelper";
 
 function App() {
   return (
-    <div>
-      <Palette palette={colorsHelper(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <h1>Welcome to the route</h1>}
+      />
+    </Switch>
+    // <div>
+    //   <Palette palette={colorsHelper(seedColors[4])} />
+    // </div>
   );
 }
 
